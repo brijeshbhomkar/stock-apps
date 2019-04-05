5cff594b9617a1f1d315d30b0a88e1418a2f3b50
P10458159 <brijesh.bhomkar@capita.co.uk>
2019-04-05 11:59:36 +0530

    deleting old project

--- stock-data-service/src/main/java/com/stocks/data/service/exception/StockServiceException.java
+++
@@ -1,9 +0,0 @@
-package com.stocks.data.service.exception;
-
-/**
- * The Stock service exception.
- */
-public class StockServiceException extends RuntimeException {
-    public StockServiceException() {
-    }
-}
