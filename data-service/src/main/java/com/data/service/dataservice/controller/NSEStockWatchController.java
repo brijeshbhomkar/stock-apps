82ec1fbd66bc3ec0d31522b91f727d371e6c89c8
P10458159 <brijesh.bhomkar@capita.co.uk>
2019-05-02 00:33:48 +0530

    Added email support

--- data-service/src/main/java/com/data/service/dataservice/controller/NSEStockWatchController.java
+++ data-service/src/main/java/com/data/service/dataservice/controller/NSEStockWatchController.java
@@ -1,6 +1,7 @@
 package com.data.service.dataservice.controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,9 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.data.service.dataservice.communication.GmailSender;
 import com.data.service.dataservice.external.NSEService;
 import com.data.service.dataservice.json.StockWatch;
 import com.data.service.dataservice.response.StockWatchResponse;
@@ -26,6 +30,146 @@ public class NSEStockWatchController {
 
 	@Autowired
 	private NSEService nseService;
+	
+	@Autowired
+	private GmailSender gmailSender;
+	
+	@GetMapping("/intraday/upstocks/{limit}")
+	public ResponseEntity<?> intradayUpstocks(@PathVariable final int limit) {
+		final List<StockWatch> stocks = findUpstocks();
+		if (CollectionUtils.isEmpty(stocks)) {
+			return ResponseEntity.badRequest().body(HttpStatus.NO_CONTENT);
+		}
+		List<StockWatch> data = stocks.stream().distinct().collect(Collectors.toList());
+		List<StockWatch> result = data.stream().sorted(Comparator.comparing(StockWatch::getTrdVol).reversed())
+				.limit(limit == 0 ? 5 : limit).collect(Collectors.toList());
+		
+		//send an email 
+		gmailSender.init(result);
+		return new ResponseEntity<List<StockWatch>>(result, HttpStatus.OK);
+	}
+
+	private List<StockWatch> findUpstocks() {
+		StockWatchResponse response = null;
+		List<StockWatch> stocks = new ArrayList<>();
+
+		// NSE_NIFTY_STOCKWATCH_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getLow()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_NEXT_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_NEXT_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getLow()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_MIDCAP_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_MIDCAP_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getLow()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_MIDCAP_150
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_MIDCAP_150);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getLow()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_SMALCAP_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_SMALCAP_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getLow()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_SMALCAP_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_SMALCAP_250);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getLow()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_MIDCAP_400
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_MIDCAP_400);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getLow()))
+					.collect(Collectors.toList()));
+		}
+		return stocks;
+	}
+
+	private List<StockWatch> findDownstocks() {
+		StockWatchResponse response = null;
+		List<StockWatch> stocks = new ArrayList<>();
+
+		// NSE_NIFTY_STOCKWATCH_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getHigh()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_NEXT_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_NEXT_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getHigh()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_MIDCAP_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_MIDCAP_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getHigh()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_MIDCAP_150
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_MIDCAP_150);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getHigh()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_SMALCAP_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_SMALCAP_50);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getHigh()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_SMALCAP_50
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_SMALCAP_250);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getHigh()))
+					.collect(Collectors.toList()));
+		}
+
+		// NSE_NIFTY_STOCKWATCH_MIDCAP_400
+		response = nseService.fetchNseStockWatch(EndpointUrls.NSE_NIFTY_STOCKWATCH_MIDCAP_400);
+		if (!CollectionUtils.isEmpty(response.getData())) {
+			stocks.addAll(response.getData().stream().filter(s -> s.getOpen().equals(s.getHigh()))
+					.collect(Collectors.toList()));
+		}
+		return stocks;
+	}
+
+	@RequestMapping("/intraday/downstocks/{limit}")
+	public ResponseEntity<?> intradayDownStocks(@PathVariable final int limit) {
+		final List<StockWatch> stocks = findDownstocks();
+		if (CollectionUtils.isEmpty(stocks)) {
+			return ResponseEntity.badRequest().body(HttpStatus.NO_CONTENT);
+		}
+		List<StockWatch> data = stocks.stream().distinct().collect(Collectors.toList());
+		List<StockWatch> result = data.stream().sorted(Comparator.comparing(StockWatch::getTrdVol).reversed())
+				.limit(limit == 0 ? 5 : limit).collect(Collectors.toList());
+		return new ResponseEntity<List<StockWatch>>(result, HttpStatus.OK);
+	}
 
 	@RequestMapping("/nifty/50")
 	public ResponseEntity<?> niftyEquity50() {
--- data-service/src/main/java/com/data/service/dataservice/json/StockWatch.java
+++ data-service/src/main/java/com/data/service/dataservice/json/StockWatch.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class StockWatch implements Serializable {
+public class StockWatch implements Serializable, Comparable<StockWatch> {
 
 	private static final long serialVersionUID = 8150197359434277644L;
 
@@ -161,6 +161,38 @@ public class StockWatch implements Serializable {
 
 	public void setIislPercChange(Double iislPercChange) {
 		this.iislPercChange = iislPercChange;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((low == null) ? 0 : low.hashCode());
+		result = prime * result + ((open == null) ? 0 : open.hashCode());
+		result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StockWatch other = (StockWatch) obj;
+		if (symbol == null) {
+			if (other.symbol != null)
+				return false;
+		} else if (!symbol.equals(other.symbol))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(StockWatch o) {
+		return trdVol.compareTo(o.trdVol);
 	}
 
 }
