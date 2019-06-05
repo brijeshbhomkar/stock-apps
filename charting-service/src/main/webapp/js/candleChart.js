79aca3e056f914366a97d948d0d5161595d73f1b
P10458159 <brijesh.bhomkar@capita.co.uk>
2019-06-05 09:24:22 +0530

    changes to chart

--- charting-service/src/main/webapp/js/candleChart.js
+++ charting-service/src/main/webapp/js/candleChart.js
@@ -6,11 +6,14 @@ $.getJSON('http://localhost:12020/api/nse/symbol/'+id, function (data) {
   // split the data set into ohlc and volume
   var ohlc = [],
     volume = [],
-    signals = [],
-    flags = [],
+    downs = [],
+    ups = [],
+    downFlags = [],
+    upFlags = [],
     dataLength = data.length,
     i = 0,
-    j = 0;
+    j = 0,
+    k = 0;
 
   for (i; i < dataLength; i += 1) {
     ohlc.push([
@@ -19,7 +22,8 @@ $.getJSON('http://localhost:12020/api/nse/symbol/'+id, function (data) {
         data[i]["high"], // high
         data[i]["low"], // low
         data[i]["close"], // close
-    	data[i]["signal"] //signal up or down
+    //	data[i]["down"], //down
+    //	data[i]["up"] //up
     ]);
 
     volume.push([
@@ -27,17 +31,30 @@ $.getJSON('http://localhost:12020/api/nse/symbol/'+id, function (data) {
          data[i]["volume"] // the volume
     ]);
     
-    signals.push([
-    	data[i]["signal"] //find the signals
+    downs.push([
+    	data[i]["down"] //find the down signals
     ])
+    
+     ups.push([
+    	data[i]["up"] //find the up signals
+    ])
+    
   }
   
-  for(j; j<signals.length; j += 1) {
-	  var signal = signals[j];
+  for(j; j<downs.length; j += 1) {
+	  var signal = downs[j];
 	  if (signal != 0) {
-		  flags.push([signal])
+		  downFlags.push([signal])
 	  }
   }
+  
+  for(k; k<ups.length; k += 1) {
+	  var signal = ups[k];
+	  if (signal != 0) {
+		  upFlags.push([signal])
+	  }
+  }
+  
   
   Highcharts.stockChart('container', {
 	
@@ -117,261 +134,514 @@ $.getJSON('http://localhost:12020/api/nse/symbol/'+id, function (data) {
     {
         type: 'flags',
         data: [{
-            x: flags[0],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[0],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[1],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[1],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[2],
-            title: 'D',
-            text: 'Down',
-        }, {
-            x: flags[3],
-            title: 'D',
-            text: 'Down',
-        },
-        {
-            x: flags[4],
-            title: 'D',
-            text: 'Down',
-        },
-        {
-            x: flags[5],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[2],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[6],
-            title: 'D',
-            text: 'Down',
-        }, {
-            x: flags[7],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[3],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[8],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[4],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[9],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[5],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[9],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[6],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[10],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[7],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[11],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[8],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[12],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[9],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[13],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[9],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[14],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[10],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[15],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[11],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[16],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[12],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[17],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[13],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[18],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[14],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[19],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[15],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[20],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[16],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[21],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[17],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[22],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[18],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[23],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[19],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[24],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[20],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[25],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[21],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[26],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[22],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[27],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[23],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[28],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[24],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[29],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[25],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[30],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[26],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[31],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[27],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[32],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[28],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[33],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[29],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[34],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[30],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[35],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[31],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[36],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[32],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[37],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[33],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[38],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[34],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[39],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[35],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[40],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[36],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[41],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[37],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[41],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[38],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[42],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[39],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[43],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[40],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[44],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[41],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[45],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[41],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[46],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[42],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[47],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[43],
+            title: 'S',
+            text: 'Stock is going down',
         }, 
         {
-            x: flags[48],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[44],
+            title: 'S',
+            text: 'Stock is going down',
         }, {
-            x: flags[49],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[45],
+            title: 'S',
+            text: 'Stock is going down',
         },
         {
-            x: flags[50],
-            title: 'D',
-            text: 'Down',
+            x: downFlags[46],
+            title: 'S',
+            text: 'Stock is going down',
+        },
+        {
+            x: downFlags[47],
+            title: 'S',
+            text: 'Stock is going down',
+        }, 
+        {
+            x: downFlags[48],
+            title: 'S',
+            text: 'Stock is going down',
+        }, {
+            x: downFlags[49],
+            title: 'S',
+            text: 'Stock is going down',
+        },
+        {
+            x: downFlags[50],
+            title: 'S',
+            text: 'Stock is going down',
+        },
+        {
+            x: upFlags[0],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[1],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[2],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[3],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[4],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[5],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[6],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[7],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[8],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[9],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[9],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[10],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[11],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[12],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[13],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[14],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[15],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[16],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[17],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[18],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[19],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[20],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[21],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[22],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[23],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[24],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[25],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[26],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[27],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[28],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[29],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[30],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[31],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[32],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[33],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[34],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[35],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[36],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[37],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[38],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[39],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[40],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[41],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[41],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[42],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[43],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[44],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[45],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[46],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[47],
+            title: 'B',
+            text: 'Stock is going up',
+        }, 
+        {
+            x: upFlags[48],
+            title: 'B',
+            text: 'Stock is going up',
+        }, {
+            x: upFlags[49],
+            title: 'B',
+            text: 'Stock is going up',
+        },
+        {
+            x: upFlags[50],
+            title: 'B',
+            text: 'Stock is going up',
         }
         ],
         onSeries: id+'-ohlc',
-        shape: 'diamond',
-        width: 20
+        shape: 'square',
+        width: 30
     }, 
     ],
     responsive: {
