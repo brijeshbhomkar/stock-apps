30e85f7260398deb2412a6cb8bba9c3fb649d91b
P10458159 <brijesh.bhomkar@capita.co.uk>
2019-09-10 14:15:08 +0530

    Added gann trading strategy

---
+++ gann-trading-strategy/src/main/java/com/strategy/gann/ganntradingstrategy/GannTradingStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.strategy.gann.ganntradingstrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GannTradingStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GannTradingStrategyApplication.class, args);
+	}
+
+}
---
+++ gann-trading-strategy/src/test/java/com/strategy/gann/ganntradingstrategy/GannTradingStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strategy.gann.ganntradingstrategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GannTradingStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
